54819849cb7fd462dda83f9920d5385d222d9f22
ihsanazharr <ihsan.azhar41@gmail.com>
2021-12-27 20:33:03 +0700

    mengubah file index dan menambahkan file style.css

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<b>tes</b>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Testing git</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+<body>
+<h1>ini ngetes aja</h1>
+</body>
+</html>
